53d737400e04630c3a5f83fa149ae3f7c117e24e
ardra04 <ardras124@gmail.com>
2022-04-06 09:39:50 +0530

    Adding UserStory Registration and Login

---
+++ UserStory_Registration-Login/CodeJavaApp/src/main/java/net/codejava/PasswordEncoder.java
@@ -0,0 +1,13 @@
+package net.codejava;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public class PasswordEncoder {
+
+	public static void main(String[] args) {
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+		String rawPassword = "qwerty@123";
+		String encodedPassword = encoder.encode(rawPassword);
+		System.out.println(encodedPassword);
+	}
+}
